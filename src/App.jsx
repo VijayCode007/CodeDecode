8a19f00c39e26075bd07454790f1cb0dd4b9f63d
Vijay Kamble <k.vijaysunanda007@gmail.com>
2024-10-26 03:05:02 +0530

    updated design with css v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,68 @@
 import React from 'react'
+import './App.css'
+import './assets/DeviconArgocd'
+import { DeviconArgocd } from './assets/DeviconArgocd'
+
+function NavBar(){
+  return (
+    <nav>
+      <div className="left">
+        <DeviconArgocd/>
+        <h2>CodeDecode</h2>
+      </div>
+      <ul className='right'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Search</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+function SideBar(){
+  return(
+    <div className='side'>
+      <ul className='strings'>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul className='encrypt'>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul className='hash'>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul className='misc'>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <div className="arrow">Collapse</div>
+    </div>
+  )
+}
+
+function MainSection(){
+  return(
+    <div className='mainSec'>MainSection</div>
+  )
+}
 
 function App() {
   return (
-    <div><h1>Hello World</h1></div>
+    <div className="cntr">
+      <NavBar/>
+      <SideBar/>
+      <MainSection/>
+    </div>
   )
 }
 
---
+++ src/Components/ShiftCipher.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShiftCipher() {
+  return (
+    <div>ShiftCipher</div>
+  )
+}
+
+export default ShiftCipher
